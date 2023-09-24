9e9e604e693a8d387c871b480354b8a9c52364ce
Andy Lu (Lu, Yu-An) <86964895+NaoCoding@users.noreply.github.com>
2023-09-24 21:07:10 +0800

    Add files via upload

---
+++ autozip_ntnu.py
@@ -0,0 +1,30 @@
+import os 
+
+homework_id = int(input("Enter which No. of homework(ex.hw01 = 1)"))
+
+common_file = ["./makefile","./README","./readme"]
+files_ = 0
+for i in range(3):
+	if(os.path.exists(common_file[i])):
+		print("Found "+common_file[i])
+		print("Saving "+common_file[i])
+		os.system("zip output "+common_file[i])
+		files_ += 1
+
+
+file_id = 1
+while(1):
+	file_path = "./hw0"+str(homework_id)+"0"+str(file_id)
+	if(os.path.exists(file_path)):
+
+		print("Found "+file_path+".c")
+		print("Saving "+file_path+".c")
+		os.system("zip output "+file_path+".c")
+		files_ += 1
+		file_id += 1
+	else:
+		break
+
+print("Auto-Zip Success!")
+print("Total zipped "+str(files_)+" files")
+
